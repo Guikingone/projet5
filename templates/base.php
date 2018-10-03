8a5477eb02b4870dd30876cd30496a16e1f2aafd
Avenir2point0 <dimitri.subrini@gmail.com>
2018-10-03 14:39:18 +0800

    some changes in style, updated README.md

--- templates/base.php
+++ templates/base.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Chakra+Petch" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <body>
@@ -59,7 +60,12 @@
     <?= $content ?>
     </div>
     <footer>
-      <p>Test footer</p>
+    <div>    
+    <p>Liens pour me suivre :<a href="https://www.linkedin.com/in/dimitri-subrini-b48848156/" class="fa fa-linkedin"></a><a href="https://www.facebook.com/profile.php?id=1152429246" class="fa fa-facebook"></a> </p>
+    </div>
+
+  </a>
+
     </footer>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
